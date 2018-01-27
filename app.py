b3d894823ccf114d160e9845136b20303113b20b
winner <wachirawit.kan@gmail.com>
2018-01-27 12:09:12 +0700

    test button template

--- app.py
+++ app.py
@@ -42,9 +42,34 @@ def reply(replyToken, textList):
     msgs = []
     for text in textList:
         msgs.append({
-            "type": "sticker",
-            "packageId": "1",
-            "stickerId": "1"
+            "type": "template",
+            "altText": "This is a buttons template",
+            "template": {
+                "type": "buttons",
+                "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
+                "imageAspectRatio": "rectangle",
+                "imageSize": "cover",
+                "imageBackgroundColor": "#FFFFFF",
+                "title": "Menu",
+                "text": "Please select",
+                "actions": [
+                    {
+                        "type": "postback",
+                        "label": "Buy",
+                        "data": "action=buy&itemid=123"
+                    },
+                    {
+                        "type": "postback",
+                        "label": "Add to cart",
+                        "data": "action=add&itemid=123"
+                    },
+                    {
+                        "type": "uri",
+                        "label": "View detail",
+                        "uri": "http://example.com/page/123"
+                    }
+                ]
+            }
         })
     data = json.dumps({
         "replyToken":replyToken,
